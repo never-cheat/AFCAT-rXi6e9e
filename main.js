eb0f3936f4c735a12f419520e7d22bc42218aa0d
never-cheat <manasvi934@gmail.com>
2025-12-29 22:18:38 +0530

    Refactor course data structure in main.js

--- main.js
+++ main.js
@@ -1,10 +1,214 @@
 const courses = [
-    {
+      {
         number: 1,
         title: "Percentage Class 1",
         category: "Maths",
         videoUrl: "https://us06web.zoom.us/rec/share/IbCBywGgKwJQNXjN3GF5l7YxmpknOKqIcazi_cxTzRloTSKbvugkTHTvxJeZjMcM.l8P7kShToCoE4gXL"
-    },
+      },
+      {
+        number: 2,
+        title: "Percentage Class 2",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/FjBAdX1f6yM3EXW-fhXAoZwsiqNhj9YmY0XD2zdGQq6EFdVkIhOaXa0fJvjAbaKG.XGOhKfdLoyo2YtZI'
+       },
+       {
+        number: 3,
+        title: "Percentage Class 3",
+        category: "Maths",
+        videoUrl:  'https://us06web.zoom.us/rec/share/A3WAQKhVn-_axkmHKL1XX3tmiNIUknmdF2rDMHhZaV2V6ERcHxeWuDkeIkLV6-1D.GfBFQ1yGtdQtJiQa'
+        },
+        {
+        number: 4,
+        title: "Percentage Class 4",
+        category: "Maths",
+        videoUrl:  'https://us06web.zoom.us/rec/share/TNO-py0x91CA2BxebWqh6kT-5vQmzNJQec0fume05-3V2i4SM1oTiyTqcBIUqVXH.9WLEyYXXTrKcssXQ'
+        },
+      {
+        number: 5,
+        title: "Ratio class + Practice sheet ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/d-qVphYnX8VSUONoh3Twd8DdX40BPcb6YznSBf3osi8ILr7T23T8IC9uMkLNtOGY.2fgB7OF-4Ipq6Via'
+      },
+      {
+        number: 6,
+        title: "Rato class 2 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/rTXSmggYk1L6r8XQHICPgIgWo-E-y6-dEktU6FtMdKV_8zA8RRbIqAEANz-5j4xm.N2xsAOOY5Nt0LtQp'
+      },
+      {
+        number: 7,
+        title: "Ratio class 3 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/MyjIRYHtS9los9vHBOyfTqb6jy4-_2QZu_634cOoPHl4TwqE-MpiY0KUFm4gIZ-P.Ok5KAizj6wUrtK30'
+      },
+      {
+        number: 8,
+        title: "Profit and loss Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/E8i1xVVmq6NokqVpSzVtc1P7s3b9TgSrV254SRMkFE6WP1eYCSvDs4l0_E1JIjTa.t1rugvIGSd97AjW2'
+      },
+      {
+        number: 9,
+        title: "Profit and loss + doubt session ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/MAXQCYgRAhlOGwws1ID9TLj7gdqY5k9xzoO1zoLZrTDBPF8cxCLhB6TgSw2IvuNz.OoFVIhmBUXF6BW9w'
+      },
+      {
+        number: 10,
+        title: "Simple interest Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/D0l3uW5pngc4Im7Sozvi9sipoGdeUBnwpO0WFDfGGnd54ZAR7QWN2wtljsov4cQY.ofMxatZzRXmi0TTy'
+      },
+      {
+        number: 11,
+        title: "Percentage Doubt Class ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/qEonpNN7qPFGKiqclf8yhRBQmLSjzNlPMYoK55kg2Q4yZfp6G7Wm7AbOrNt1G3Z3.qWzKpXp08iSG13jI'
+      },
+      {
+        number: 12,
+        title: "Ratio Doubt Class ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/UVw8-y-BlS2W7tNFEvroZp8ca4SKoTeO7Hjz_7xWZKADBo0Qr3COpJ6ML4P1AW5Q.k5AfaNqC8jOwaNcY'
+      },
+      {
+        number: 13,
+        title: "SI + Profit and loss doubt session ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/2DtUpKWl-96tPfqFUzd-2mXWtz_ajeXZ0t18y5gYNvDN55YoWzVjnyGzNusasP3W.5daM-0pfG6dY9JDu'
+      },
+      {
+        number: 14,
+        title: "CI + SI doubts",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/NadQCyheOriqEPSiPbbEhEo10bxxHpzxcnH0OpPjB6YeX9YixpLT62CaihWw0J-6.ilRmuEMh9CP0bhML'
+      },
+      {
+        number: 15,
+        title: "CI Questions ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/tif-Y3MER9LtyHB53EVsYyxckyGfkywLMPuwCz4eYC8kjH0v-bttTUHN9piqoHbA.U08ycACqQ5S2v4TM'
+      },
+      {
+        number: 16,
+        title: "CI Doubts ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/bmdV2bkm9caKdIY6iwpeiTKXCRapCSkFTqLRnztj-dRW2g8NHS2nGX-pp7f8H72v.XobQ6AlHHradH3RI'
+      },
+{
+        number: 17,
+        title: "Speed, Distance and Time Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/7v8BiXUg-0Yyn2JQz0QueElnDjIhLjckNB6wMrf_Et5g0605F5a0U0IOPNaH9E71.EXTv6iRfLe70lFdK'
+      },
+     {
+        number: 18,
+        title: "Speed,Distance And Time Class 2 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/OfQ9YDrE1npZ-FDIho-eD9uSUXm5rPh7NOhnJOyqt6gnTEWPZft7DgKqETN5-Uc3.11ZTh9t9HreyOXfw'
+      },
+      {
+        number: 19,
+        title: "Speed Distance time type 1-4 doubts ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/5pTcKNt10xoX-HM3WpLMr_Skkh99IbNR25o34Xi1aMyBlvrCAySV_RBojxX30fgk.zI1qHwJ6NS952iEp'
+      },
+      {
+        number: 20,
+        title: "Boat & stream + SDT Type 5 doubts ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/LIeEjwv73uV_7ILHs9VsXS-WbpxY4aEb0erewCToeJ2y2wBEUDco9BkApVfqhRRk.QbRswrCeeq75KYm9'
+      },
+     {
+        number: 21,
+        title: "SDT complete ",
+        category: "Maths",
+        videoUrl:  'https://us06web.zoom.us/rec/share/0NMviAc9xVyrdq-yKk3mzh3GVhAkEh5dgLIhPsl6-b7MBP86vsvICIGC4x5fxT6G.50AwwggD7gcUvd-f'
+      },
+      {
+        number:22 ,
+        title: "Boat and streams boubt ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/nW95Eshp28r5rDH2nyeZ56QkITO7kyDHq0vw3cFH5v2HabuLm_OMY3oI2u06wuhK.qcHgjO4Z_irO0Mew'
+      },
+      {
+        number: 23,
+        title: "Time & Work - Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/shKhxRbl9z2HVNcY4Ly_GgmACZ-8CzkEDnXbuR4wjE_sZRs6IGCsbZYfUmh9fnyX.WdekCJPThlORVL6l'
+      },
+      {
+        number: 24,
+        title: "Time & Work - Class 2 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/dhC9Mh09MCnmyJcZNc2wFUZjatDdvFJsR61VEOiL4ZMZVXlt81QF4dssESA8tHF9.VRWdOxLFrgdvldJv'
+      },
+      {
+        number: 25,
+        title: "Time and Work - Class 3 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/NQki2HoQ9XRlr_tHSZHLDMmZec-IzKsC8Z96zLTwWsp_R0bW0OOjMlgPZCxb8FuV.DqLSZzBe0yNafb8k'
+      },
+      {
+        number: 26,
+        title: "Pipe & Cistern - Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/WNHTCKegzGjs1teIzML77sf2vCKl2mna0AQWT7flqf6ksjPuMlNrBcP8H0jpZQYW._6NTI_PPlexsfjCO'
+      },
+      {
+        number: 27,
+        title: "Area and Perimeter - Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/eLy3ekzdgdQdY0d4zwyH4hUu8Lt5Dp5MwoGegCp1ZIAV-D_5QJ5FmfcPNjfxjPTa.8xNoD8FmMT6nLB29'
+      },
+      {
+        number: 28,
+        title: "Area and Perimeter - Class 2 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/ScjjBuCMj64tzamzUSzUP5MRtgbkt2Yeh4OaCTmlkXpCRmC08w6mekhJfLDZi9Y.W8h-x4_Bjwi2o9e3'
+      },
+      {
+        number: 29,
+        title: "Height and Distance Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/IF5060hRouPfz3EE7eJ17RR_L8AxWAgUib08D8yMisaHiDm7hhJnBItXUFWANCw.gbByTAjr-HI-qxnD'
+      },
+      {
+        number: 30,
+        title: "Height and distance Class 2 + Area and Perimeter Doubts ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/pMKcO_iMWHC97taxmU1ntHwGGZKwzUm2O89ymBsbqryGGyWzZELjp4nBcJPc-hCB.q9n0IXc_RiQIjZeo'
+      },
+      {
+        number: 31,
+        title: "Number system Class 1 ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/tUNC1eKhSpjIQAtF-Pudb5_0oVC06YG0e-fMVlz-NqzWQa1i3i3NErfT504mJKzC.89HW-ljewMYgfNl7'
+      },
+      {
+        number: 32,
+        title: "Number system - Divisibility + Height & distance Doubt ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/gpHVvuHMSnq18xupejvvKYWdApwktD2gRjIutssOHtei9QYW0yH_q_xhdfRWMv19.5SeiklNgoS_pGiY9'
+      },
+      {
+        number: 33,
+        title: "Number System - Fraction & Bar + Height & Distance ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/FGQwmtCNag_FA4F1gDf0fNv_esE6WZvKTR6SxUbcc7xkrmnX0eWyVFBzmEXWUjWf.T6lNk1JJxvGRZJyS'
+      },
+      {
+        number: 34,
+        title: "Number system ( Remainder Theorem ) ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/q4sjWeAoz40KIwQDj5eveALpOD2n4CigvBgIckeYncIfxXM2eBtQcj27kD9wc4Pz.DJAwn7FW_cEoclDd'
+      },
+      {
+        number: 35,
+        title: "Number System - Remainder Theorem + Doubts ",
+        category: "Maths",
+        videoUrl: 'https://us06web.zoom.us/rec/share/C2nrzb-NwClYtVrll1Yl-oNCHs7zIBz3WCO7B3fg4PfdFghc3gdy90BsKLRdGlrr.RFZi-fz_XFv3FCyf'
+      }
  ]; 
 
 let currentCategory = 'all';
